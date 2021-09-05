31db8f5e34d6a830d9aec7f451e7f51874d2fc94
changpei <changpei@meituan.com>
2021-09-05 17:56:39 +0800

    fix: 1

---
+++ .history/commitlint.config_20210905175554.js
@@ -0,0 +1,6 @@
+/*
+ * @Author: 常培
+ * @Date: 2021-09-05 17:55:53
+ * @LastEditors: 常培
+ * @LastEditTime: 2021-09-05 17:55:54
+ */
---
+++ commitlint.config.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: 常培
+ * @Date: 2021-09-05 17:55:53
+ * @LastEditors: 常培
+ * @LastEditTime: 2021-09-05 17:55:54
+ */
+module.exports = {
+  extends: ['@commitlint/config-conventional']
+}
